4261127ec15b7f6b9a33ba119191c2f7632f8ac6
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-12-29 13:30:40 -0500

    Readjust

--- server/server.js
+++ server/server.js
@@ -2,18 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const http = require('http')
-const server = http.createServer(app)
 
-if (process.env.NODE_ENV === 'production') {
-	// Exprees will serve up production assets
-	app.use(express.static('client/build'));
-  
-	// Express serve up index.html file if it doesn't recognize route
-	const path = require('path');
-	app.get('*', (req, res) => {
-	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+});
+
+
+const server = http.createServer(app)
 
 const io = require("socket.io")(server, {
 	cors: {
